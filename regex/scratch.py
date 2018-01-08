3002e8fd21209eb17efdc3396701096d71ca0718
Marco Hemken <mehemken@gmail.com>
2018-01-08 08:40:58 -0800

    new regex page on substitutions

--- regex/scratch.py
+++ regex/scratch.py
@@ -7,5 +7,23 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG)
 
 
+msg = ['qa011_duplicate_individuals.sql',
+    'QA010_middle_initial_in_first_name.sql',
+    'qa910_something_utterly_useless.sql',
+    'qa9876_malformed.sql',
+    'QA472_probably_not_needed.sql']
+
 
 if __name__ == '__main__':
+
+    p = re.compile(r'(qa[0-9]{3}(?=\D))(\w*).sql', re.I)
+    print('p')
+    for i in msg:
+        s = p.sub(r'\1_DISCONTINUED\2.sql', i)
+        print(s)
+
+    # p = re.compile(r'(qa[0-9]{3})(?=\D)', re.I)
+    # print('p')
+    # for i in msg:
+    #     s = p.sub('QA000', i)
+    #     print(s)
