be5952485185b0101b4d7e5c26def075f3ea6dcb
Marco Hemken <mehemken@gmail.com>
2018-01-09 07:25:53 -0800

    finished the regex tutorial

--- regex/scratch.py
+++ regex/scratch.py
@@ -13,6 +13,8 @@ msg = ['qa011_duplicate_individuals.sql',
     'qa9876_malformed.sql',
     'QA472_probably_not_needed.sql']
 
+html = '<html><head></head><body><h1>Hello!</h1></body></html>'
+
 
 if __name__ == '__main__':
 
@@ -22,8 +24,7 @@ if __name__ == '__main__':
         s = p.sub(r'\1_DISCONTINUED\2.sql', i)
         print(s)
 
-    # p = re.compile(r'(qa[0-9]{3})(?=\D)', re.I)
-    # print('p')
-    # for i in msg:
-    #     s = p.sub('QA000', i)
-    #     print(s)
+    q = re.compile(r'<.*?>')
+
+    foo = q.match(html)
+    print(foo.group())
